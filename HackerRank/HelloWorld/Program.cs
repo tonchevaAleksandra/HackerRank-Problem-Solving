7f54285e4a182aade029c7eb5dcaa5e2e6397af8
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-13 13:24:28 +0300

    Add file to 30DaysChallenge

---
+++ HackerRank/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            String inputString;
+            inputString = Console.ReadLine();
+            Console.WriteLine("Hello, World.");
+            Console.WriteLine(inputString);
+
+        }
+    }
+}
