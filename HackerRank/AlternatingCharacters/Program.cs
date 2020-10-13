5c4ccde1d6c1f95a5a6d5b4e77f91a34a0a75f75
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-13 22:01:33 +0300

    Add files to HackerRank

---
+++ HackerRank/AlternatingCharacters/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AlternatingCharacters
+{
+    class Program
+    {
+        static int alternatingCharacters(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            stack.Push(s[0]);
+
+            for (int i = 1; i < s.Length; i++)
+            {
+               var currChar = s[i];
+               if(currChar=='A'  && stack.Peek()=='B')
+                {
+                    stack.Push(currChar);
+                }
+               else if(currChar=='B' && stack.Peek()=='A')
+                {
+                    stack.Push(currChar);
+                }
+            }
+            return s.Length - stack.Count();
+        }
+        static void Main(string[] args)
+        {
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                int result = alternatingCharacters(s);
+
+                Console.WriteLine(result);
+            }
+        }
+
+    }
+}
---
+++ HackerRank/StringsMakingAnagrams/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringsMakingAnagrams
+{
+    class Program
+    {
+        static int makeAnagram(string a, string b)
+        {
+            StringBuilder sb = new StringBuilder();
+            var length = a.Length + b.Length;
+            var first = new List<char>(a);
+            var second = new List<char>(b);
+            for (int i = 0; i < first.Count; i++)
+            {
+                if (second.Contains(first[i]))
+                {
+                    var currC = first[i];
+                    sb.Append(currC);
+                    first.RemoveAt(i);
+                    var index = second.IndexOf(currC);
+                    second.RemoveAt(index);
+                    i--;
+                }
+
+            }
+            return length - (2 * sb.Length);
+
+        }
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            string b = Console.ReadLine();
+
+            int res = makeAnagram(a, b);
+
+            Console.WriteLine(res);
+
+        }
+    }
+}
