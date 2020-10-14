44ed78b533fc7e59fdcbf0cb5dbb87cc75a5773a
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-14 22:07:17 +0300

    Add files to HackerRank

---
+++ HackerRank/Balancedbrackets/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Balancedbrackets
+{
+    class Program
+    {
+        static string isBalanced(string s)
+        {
+            Stack<char> brackets = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var currChar = s[i];
+                switch (currChar)
+                {
+                    case '(':
+                    case '{':
+                    case '[':
+                        brackets.Push(currChar);
+                        break;
+                    case ')':
+                        if (brackets.Any() && brackets.Peek() == '(')
+                            brackets.Pop();
+                        else return "NO";
+                        break;
+                    case '}':
+                        if (brackets.Any() && brackets.Peek() == '{')
+                            brackets.Pop();
+                        else return "NO";
+                        break;
+                    case ']':
+                        if (brackets.Any() && brackets.Peek() == '[')
+                            brackets.Pop();
+                        else return "NO";
+                        break;
+
+                }
+            }
+
+            return !brackets.Any() ? "YES" : "NO";
+
+        }
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string s = Console.ReadLine();
+
+                string result = isBalanced(s);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ HackerRank/DataTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declare second integer, double, and String variables.
+            int a = 4;
+            double b = 4.0;
+            string c = "HackerRank ";
+            // Read and save an integer, double, and String to your variables.
+            int n = int.Parse(Console.ReadLine());
+            double m = double.Parse(Console.ReadLine());
+            string k = Console.ReadLine();
+
+            Console.WriteLine(a+n);
+            Console.WriteLine($"{b+m:F1}");
+            Console.WriteLine(c+k);
+
+            // Print the sum of both integer variables on a new line.
+
+            // Print the sum of the double variables on a new line.
+
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+
+        }
+    }
+}
