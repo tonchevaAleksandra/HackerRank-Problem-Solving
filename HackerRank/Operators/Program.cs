d5a279b2c0b18bf781850897375c3b9b8d8feae9
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-15 16:05:59 +0300

    Add files to HackerRank

---
+++ HackerRank/Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static decimal solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+            decimal price = (decimal)(meal_cost + (meal_cost * tip_percent) / 100);
+            price += (decimal)((meal_cost * tax_percent) / 100);
+            return price;
+        }
+        static void Main(string[] args)
+        {
+            double meal_cost = Convert.ToDouble(Console.ReadLine());
+
+            int tip_percent = Convert.ToInt32(Console.ReadLine());
+
+            int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+           decimal totalPrice= solve(meal_cost, tip_percent, tax_percent);
+            Console.WriteLine(Math.Round(totalPrice));
+        }
+    }
+}
---
+++ HackerRank/QueriesAtaleOfTwoStacks/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QueriesAtaleOfTwoStacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] targets = Console.ReadLine()
+             .Split()
+             .Select(int.Parse)
+             .ToArray();
+                var command = targets[0];
+                switch (command)
+                {
+                    case 1:
+                        stack.Push(targets[1]);
+                        queue.Enqueue(targets[1]);
+                        break;
+                    case 2:
+                        if (queue.Any()) queue.Dequeue();
+                        break;
+                    case 3:
+                        Console.WriteLine(queue.Peek());
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            while (queue.Any())
+            {
+
+                var command = queue.Dequeue();
+                switch (command)
+                {
+                    case 1:
+                       
+                        break;
+                    case 2:
+                       
+                        break;
+                    case 3:
+                       
+                        break;
+                    default:
+                        break;
+                } 
+            }
+        }
+    }
+}
