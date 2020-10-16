bb756928620349ff4a62872fed88e9a9a3163070
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-17 00:38:48 +0300

    Add files to HackerRank

---
+++ HackerRank/IntroToConditionalStatements/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IntroToConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N % 2 != 0 || N % 2 == 0 && N >= 6 && N <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (N % 2 == 0)
+            {
+                if (N >= 2 && N <= 5 || N > 20)
+                    Console.WriteLine("Not Weird");
+            }
+
+        }
+    }
+}
