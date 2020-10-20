dd6e249d456152a8696128b1ee46dfb990e0dec6
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-20 11:38:47 +0300

    Add files to HackerRank

---
+++ HackerRank/Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+                Array.Reverse(arr);
+            Console.WriteLine(string.Join(" ",arr));
+            
+        }
+    }
+}
