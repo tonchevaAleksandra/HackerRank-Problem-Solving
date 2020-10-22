5a8a03cecee344cebade15c040a542c1447804ff
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-22 11:56:23 +0300

    Add files to HackerRank

---
+++ HackerRank/Recursion3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Recursion3
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * (Factorial(n - 1));
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = Factorial(n);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
