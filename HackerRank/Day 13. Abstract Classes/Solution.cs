6a5c051e142bdb6b4b3f5c6960ff643a0b1305e6
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-19 21:58:46 +0200

    Add files to HackerRank

---
+++ HackerRank/Day 13. Abstract Classes/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Day_13._Abstract_Classes
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }
+    }
+
+    class MyBook:Book
+    {
+      private  int price;
+        public MyBook(string t, string a, int price) 
+            : base(t, a)
+        {
+            this.price = price;
+        }
+
+        public override void display()
+        {
+
+            Console.WriteLine($"Title: {this.title}");
+            Console.WriteLine($"Author: {this.author}");
+            Console.WriteLine($"Price: {this.price}");
+        }
+    }
+    abstract class Book
+    {
+
+        protected String title;
+        protected String author;
+
+        public Book(String t, String a)
+        {
+            title = t;
+            author = a;
+        }
+        public abstract void display();
+
+
+    }
+}
