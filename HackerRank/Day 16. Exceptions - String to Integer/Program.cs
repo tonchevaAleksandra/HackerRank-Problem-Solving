d6b739656498341f13ad2fa7649cba421d3f3d96
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2021-01-19 23:08:56 +0200

    Add files to HackerRank.sln

---
+++ HackerRank/Day 16. Exceptions - String to Integer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day_16._Exceptions___String_to_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(int.Parse(S));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
