da25a7c369a6ef96d15be57590b4bd70fc446bb0
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-02-11 12:35:57 +0200

    Add files to HackerRank Skills Certification Test

---
+++ C Sharp Basic Skills Certification Test/1. Problem/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading;
+
+namespace _1._Problem
+{
+  public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!typeof(Subteam).IsSubclassOf(typeof(Team)))
+            {
+                throw new Exception("Subteam class should inherit from Team class");
+            }
+
+            String str = Console.ReadLine();
+            String[] strArr = str.Split();
+            string initialName = strArr[0];
+            int count = Convert.ToInt32(strArr[1]);
+            Subteam teamObj = new Subteam(initialName, count);
+            Console.WriteLine("Team " + teamObj.teamName + " created");
+
+            str = Console.ReadLine();
+            count = Convert.ToInt32(str);
+            Console.WriteLine("Current number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+            teamObj.AddPlayer(count);
+            Console.WriteLine("New number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+
+
+            str = Console.ReadLine();
+            count = Convert.ToInt32(str);
+            Console.WriteLine("Current number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+            var res = teamObj.RemovePlayer(count);
+            if (res)
+            {
+                Console.WriteLine("New number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+            }
+            else
+            {
+                Console.WriteLine("Number of players in team " + teamObj.teamName + " remains same");
+            }
+
+            str = Console.ReadLine();
+            teamObj.ChangeTeamName(str);
+            Console.WriteLine("Team name of team " + initialName + " changed to " + teamObj.teamName);
+        }
+    }
+
+  public class Team
+  {
+      public Team(string teamName, int noOfPlayers)
+      {
+          this.teamName = teamName;
+            this.noOfPlayers = noOfPlayers;
+      }
+      public string teamName { get; set; }
+      public int noOfPlayers { get; set; }
+
+      public  void AddPlayer(int count)
+      {
+          this.noOfPlayers += count;
+      }
+
+      public bool RemovePlayer(int count)
+      {
+          if (count > this.noOfPlayers)
+              return false;
+
+          this.noOfPlayers -= count;
+          return true;
+      }
+
+
+  }
+
+  public class Subteam : Team
+  {
+      public Subteam(string teamName, int noOfPlayers) 
+          : base(teamName, noOfPlayers)
+      {
+          this.teamName = teamName;
+          this.noOfPlayers = noOfPlayers;
+        }
+
+      public void ChangeTeamName(string name)
+      {
+          this.teamName = name;
+      }
+  }
+}
---
+++ C Sharp Basic Skills Certification Test/2. Employees Management/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Employees_Management
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfEmployees = int.Parse(Console.ReadLine());
+
+            var employees = new List<Employee>();
+
+            for (int i = 0; i < countOfEmployees; i++)
+            {
+                string str = Console.ReadLine();
+                string[] strArr = str.Split(' ');
+                employees.Add(new Employee
+                {
+                    FirstName = strArr[0],
+                    LastName = strArr[1],
+                    Company = strArr[2],
+                    Age = int.Parse(strArr[3])
+                });
+            }
+
+            foreach (var emp in AverageAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The average age for company {emp.Key} is {emp.Value}");
+            }
+
+            foreach (var emp in CountOfEmployeesForEachCompany(employees))
+            {
+                Console.WriteLine($"The count of employees for company {emp.Key} is {emp.Value}");
+            }
+
+            foreach (var emp in OldestAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The oldest employee of company {emp.Key} is {emp.Value.FirstName} {emp.Value.LastName} having age {emp.Value.Age}");
+            }
+        }
+
+        public static Dictionary<string, int> AverageAgeForEachCompany(List<Employee> employees)
+        {
+            var dict = new Dictionary<string, int>();
+            foreach (var emp in employees)
+            {
+                if (!dict.ContainsKey(emp.Company))
+                {
+                    string company = emp.Company;
+                    int avgAge = (int)Math.Round(employees.Where(e => e.Company == company).Average(e => e.Age));
+                    dict.Add(company, avgAge);
+                }
+            }
+
+            return dict.OrderBy(a => a.Key).ToDictionary(a => a.Key, a => a.Value); 
+        }
+
+        public static Dictionary<string, int> CountOfEmployeesForEachCompany(List<Employee> employees)
+        {
+            var dict = new Dictionary<string, int>();
+            foreach (var emp in employees)
+            {
+                string company = emp.Company;
+                if (!dict.ContainsKey(company))
+                {
+                    int count = employees.Where(e => e.Company == company).ToList().Count();
+                    dict.Add(company, count);
+                }
+            }
+
+            return dict.OrderBy(a=>a.Key).ToDictionary(a=>a.Key, a=>a.Value);
+        }
+
+        public static Dictionary<string, Employee> OldestAgeForEachCompany(List<Employee> employees)
+        {
+            var dict = new Dictionary<string, Employee>();
+            foreach (var emp in employees)
+            {
+                string company = emp.Company;
+                if (!dict.ContainsKey(company))
+                {
+                    Employee oldestAge = employees.Where(e => e.Company == company).OrderByDescending(e => e.Age).First()
+                        ;
+
+                    dict.Add(company,oldestAge);
+                }
+            }
+
+            return dict.OrderBy(a => a.Key).ToDictionary(a => a.Key, a => a.Value); ;
+        }
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public int  Age { get; set; }
+    }
+}
