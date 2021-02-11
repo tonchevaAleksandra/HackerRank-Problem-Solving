234abcc7d61a4424e32885fcc84a561bf4c9dff7
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-02-11 19:33:27 +0200

    Add files to HackerRankPractice.sln

---
+++ HackerRankPractice/TheHurdleRace/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using static TheHurdleRace.Solution;
+namespace TheHurdleRace
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nk = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            int[] height = Array.ConvertAll(Console.ReadLine().Split(' '), heightTemp => Convert.ToInt32(heightTemp))
+                ;
+            int result = hurdleRace(k, height);
+
+            Console.WriteLine(result);
+
+        }
+    }
+
+   public class Solution
+    {
+
+        // Complete the hurdleRace function below.
+      public  static int hurdleRace(int k, int[] height)
+        {
+
+            int maxHeight = height.Max();
+            if (k > maxHeight)
+                return 0;
+            return maxHeight - k;
+        }
+    }
+}
