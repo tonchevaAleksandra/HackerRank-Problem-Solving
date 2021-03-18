0029ff9bae008d196e35937dd8e5bd08a6d63092
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-18 14:53:48 +0200

    Add

--- MicroInvestInterviewExam/SecondProblem/Program.cs
+++ MicroInvestInterviewExam/SecondProblem/Program.cs
@@ -8,6 +8,14 @@ namespace SecondProblem
     {
         static void Main(string[] args)
         {
+
+            //2. Problem 2
+            //Да се намерят първите две числа, които правят дадена сума, когато бъдат събрани.Двете числа се
+            //    съдържат в масив, който заедно със числото, което репрезентира сумата, се подават от
+            //клавиетурата.Трябва да се принтират индексите на двете числа.
+            //     Как би се решила задачата със сложност по време N
+
+
             Console.WriteLine("Hello, give me an array of numbers and on a new line a number which represents a sum of two other numbers in the array!");
             Console.WriteLine("And I will tell you the indices of the first two numbers that meet the condition!");
 
