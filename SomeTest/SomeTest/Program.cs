928d937df8c26278569c748534683ddc5b947ef6
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-23 09:51:06 +0200

    Add

---
+++ SomeTest/SomeTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace SomeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(IsDescending(arr));
+        }
+        public static bool IsDescending(int[] array) { return IsDescendingOrder(array, 0); }
+
+        private static bool IsDescendingOrder(int[] array, int index)
+        {
+            if (index >= array.Length-1)
+                return true;
+            else if (array[index] <= array[index + 1])
+                return false;
+            else
+                return IsDescendingOrder(array, index + 1);
+        }
+    }
+}
