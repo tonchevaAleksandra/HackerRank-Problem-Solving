c0e0e343cd67dc630f095baf16825e9efefc33c1
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-28 19:53:44 +0300

    Rename

---
+++ Interview Problems4/EndavaExam/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace EndavaExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = null;
+            if (value.Equals(null))
+            {
+                return true;
+            }
+        }
+
+        private static Sum(params int[] values)
+        {
+
+        }
+    }
+}
