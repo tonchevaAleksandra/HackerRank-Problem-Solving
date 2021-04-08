1848730950a791f636786ea59093d4b8c1792831
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-04-08 18:04:18 +0300

    Add searching algorithm in binary tree Generic

---
+++ TreeImplementation/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SearchOccurenciesInBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new TreeNode<int>();
+            tree.Value = 1;
+            tree.LefChild = new TreeNode<int>()
+            {
+                Value = 2,
+                LefChild = new TreeNode<int>() { Value = 1, LefChild = new TreeNode<int>() { Value = 3 } },
+                RightChild = new TreeNode<int>() { Value = 5 }
+            };
+            tree.RightChild = new TreeNode<int>() { Value = 2 };
+            var count = 0;
+            bool isThereOccurencies = FindOccurencies(tree, 1, ref count);
+            Console.WriteLine(isThereOccurencies);
+        }
+
+        public static bool FindOccurencies(TreeNode<int> treeNode, int value, ref int count)
+        {
+
+            if (treeNode.Value == value)
+            {
+                count++;
+            }
+
+            if (treeNode.LefChild != null)
+            {
+                FindOccurencies(treeNode.LefChild, value, ref count);
+            }
+            if (treeNode.RightChild != null)
+            {
+                FindOccurencies(treeNode.RightChild, value, ref count);
+            }
+
+
+            return count >= 2 ? true : false;
+        }
+    }
+}
---
+++ TreeImplementation/ConsoleApp1/ConsoleApp1/TreeNode.cs
@@ -0,0 +1,10 @@
+﻿namespace SearchOccurenciesInBinaryTree
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; set; }
+        public TreeNode<T> LefChild { get; set; }
+        public TreeNode<T> RightChild { get; set; }
+
+    }
+}
